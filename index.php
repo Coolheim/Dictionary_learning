988b66566c0532ec070cf9f6f4706bdb5e2e8f09
Coolheim <kulheimmatej@gmail.com>
2024-11-27 22:06:55 +0100

    adsf

--- index.php
+++ index.php
@@ -17,6 +17,11 @@
 <body>
     <header class="header">
         <a href="index.php" class="logo">Dictionary Learning</a>
+        <div class="centered-buttons">
+            <a href="" class="classy-btn">About me</a>
+            <a href="" class="classy-btn">Privacy Policy</a>
+        </div>
+
         <div class="header-buttons">
             <a href="database/login_register/login.php" class="btn">Sign In</a>
             <a href="database/login_register/registration.php" class="btn">Sign Up</a>
@@ -26,6 +31,15 @@
     <div class="dashboard-container">
         <h1 class="dashboard-title">Welcome to your Dashboard</h1>
         <a href="database/login_register/logout.php" class="logout-btn">Logout</a>
+    </div>
+
+    <div class="links-container">
+        <div class="links">
+            <a href="" class="link-btn">Dictionary Settings</a>
+            <a href="" class="link-btn">Learning</a>
+            <a href="" class="link-btn">Check your understanding</a>
+        </div>
+        <img src="img/dictionaries.png" alt="Dictionaries" class="dashboard-image">
     </div>
 </body>
 </html>
